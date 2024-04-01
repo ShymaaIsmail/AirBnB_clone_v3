c68c630238bf749fab0f3bfc3c1967b54cb6cd8e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-02 01:13:54 +0300

    users

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,71 +1,71 @@
 #!/usr/bin/python3
-""" Users module"""
+""" Amenities module"""
 from flask import abort, jsonify, request
 from models import storage
-from models.user import User
+from models.amenity import Amenity
 from . import app_views
 
 
-@app_views.route('/users', strict_slashes=False)
-def get_users():
-    """returns all users"""
-    users_list = storage.all(User)
-    users_dicts = [v.to_dict() for k, v in users_list.items()]
-    return jsonify(users_dicts)
+@app_views.route('/amenities', strict_slashes=False)
+def get_amenities():
+    """returns all amenities"""
+    amenities_list = storage.all(Amenity)
+    amenities_dicts = [v.to_dict() for k, v in amenities_list.items()]
+    return jsonify(amenities_dicts)
 
 
-@app_views.route('/users/<user_id>', strict_slashes=False)
-def get_user(user_id=None):
-    """returns user by user_id"""
-    user = storage.get(User, user_id)
-    if user is not None:
-        return jsonify(user.to_dict())
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False)
+def get_amenity(amenity_id=None):
+    """returns amenity by amenity_id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is not None:
+        return jsonify(amenity.to_dict())
     else:
         abort(404)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'],
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_user(user_id=None):
-    """returns user by user_id"""
-    user = storage.get(User, user_id)
-    if user is not None:
-        storage.delete(user)
+def delete_amenity(amenity_id=None):
+    """returns amenity by amenity_id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is not None:
+        storage.delete(amenity)
         storage.save()
         return jsonify({}), 200
     else:
         abort(404)
 
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-def post_user():
-    """post user to storage"""
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+    """post amenity to storage"""
     if request.is_json:
         request_body = request.get_json()
         if "name" in request_body:
-            new_user = User(**request_body)
-            storage.new(new_user)
+            new_amenity = Amenity(**request_body)
+            storage.new(new_amenity)
             storage.save()
-            return jsonify(new_user.to_dict()), 201
+            return jsonify(new_amenity.to_dict()), 201
         else:
             abort(400, "Missing name")
     else:
         abort(400, "Not a JSON")
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'],
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-def put_user(user_id=None):
-    """put user to storage"""
+def put_amenity(amenity_id=None):
+    """put amenity to storage"""
     if request.is_json:
         request_body = request.get_json()
-        existed_user = storage.get(User, user_id)
-        if existed_user is not None:
+        existed_amenity = storage.get(Amenity, amenity_id)
+        if existed_amenity is not None:
             for key, value in request_body.items():
-                if key not in ['id', 'created_at', 'updated_at', 'email']:
-                    setattr(existed_user, key, value)
-            existed_user.save()
-            return jsonify(existed_user.to_dict()), 200
+                if key not in ['id', 'created_at', 'updated_at']:
+                    setattr(existed_amenity, key, value)
+            existed_amenity.save()
+            return jsonify(existed_amenity.to_dict()), 200
         else:
             abort(404)
     else:
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,15 +6,15 @@ from models.user import User
 from . import app_views
 
 
-@app_views.route('/users', strict_slashes=False)
-def get_users():
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_all_users():
     """returns all users"""
     users_list = storage.all(User)
     users_dicts = [v.to_dict() for k, v in users_list.items()]
     return jsonify(users_dicts)
 
 
-@app_views.route('/users/<user_id>', strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id=None):
     """returns user by user_id"""
     user = storage.get(User, user_id)
