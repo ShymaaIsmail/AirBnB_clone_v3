53896d90d3b1c1a5ae413d2647fa74d7b7b22f60
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-01 19:56:24 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ Index module"""
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.place import Place
 from . import app_views
-
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -13,4 +19,11 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """returns stats"""
-    return None
+    return jsonify(
+    amenities = storage.count(Amenity), 
+    cities = storage.count(City),
+    places = storage.count(Place), 
+    reviews = storage.count(Review), 
+    states = storage.count(State), 
+    users = storage.count(User)
+    )
