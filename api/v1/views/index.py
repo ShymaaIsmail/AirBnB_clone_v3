72a3d69ed10aac9d6b3a980dbd7c0acb2913dea7
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-01 19:36:02 +0300

    status api

---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask import jsonify
+from . import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns status"""
+    return jsonify({"status": "ok"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns stats"""
+    return None
