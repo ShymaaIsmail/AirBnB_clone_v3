f6d01013af73c1eb9bf0fadee76afa6c99bf46ee
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-02 02:50:05 +0300

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,15 @@
 """API Module v1"""
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
+# Allow CORS for all routes (/*) from 0.0.0.0
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
