3ea474d63ba6342aafcf01f03e3d966b99520158
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-01 19:58:40 +0300

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,6 +10,7 @@ from models.user import User
 from models.place import Place
 from . import app_views
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns status"""
@@ -20,10 +21,10 @@ def status():
 def stats():
     """returns stats"""
     return jsonify(
-    amenities = storage.count(Amenity), 
-    cities = storage.count(City),
-    places = storage.count(Place), 
-    reviews = storage.count(Review), 
-    states = storage.count(State), 
-    users = storage.count(User)
+        amenities=storage.count(Amenity),
+        cities=storage.count(City),
+        places=storage.count(Place),
+        reviews=storage.count(Review),
+        states=storage.count(State),
+        users=storage.count(User),
     )
