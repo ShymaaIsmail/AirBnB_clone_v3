2e3a11eeb831d6bc761fe5c9f4c7dc2dcf25baa2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-02 14:01:26 +0300

    test user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -66,6 +66,8 @@ def put_user(user_id=None):
             if key not in ['id', 'email', 'created_at', 'updated_at']:
                 setattr(existed_user, key, value)
         existed_user.save()
+        existed_user.places = None
+        existed_user.reviews = None
         return jsonify(existed_user.to_dict()), 200
     else:
         abort(404)
